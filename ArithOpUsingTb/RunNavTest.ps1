5731c917114fc65912776a3eff278e67ebaf46a1
Almahmud Tareq <almahmudcse@gmail.com>
2022-10-31 19:51:55 +0600

    Update RunNavTest.ps1
    gffg

--- ArithOpUsingTb/RunNavTest.ps1
+++ ArithOpUsingTb/RunNavTest.ps1
@@ -34,11 +34,11 @@ if($UseDocker -eq $true) {
     $ec = Start-B365NAVInConsoleMode @Parameters
 } else {
     $Parameters = @{
-        "HostName"      = "localhost"
+        "HostName"      = "http://localhost:8080/"
         "Instance"      = "BC190"
         "Company"       = "CRONUS Danmark"
         "ObjectType"    = "CodeUnit"
-        "ObjectID"      = $50105
+        "ObjectID"      = 50105
         "ClientPath"    = "C:\Program Files\Microsoft Dynamics 365 Business Central\190\Service\Microsoft.Dynamics.Nav.Server.exe"
     }
     $ec = Start-B365NAVInConsoleMode @Parameters
